5bcfa6927e64d9b43f19f6ae1fb4a5f51b222b3f
Kajoyrie P <32532509+GitKj@users.noreply.github.com>
2022-06-30 18:39:43 -0700

    changed to proper secret key

--- twitter_client.py
+++ twitter_client.py
@@ -3,6 +3,6 @@ import os
 
 def get_Twitter_Client():
     return tweepy.Client(
-        consumer_key=os.getenv("TWITTER_API_KEY"), consumer_secret=os.getenv("TWITTER_SECRET_KEY"),
+        consumer_key=os.getenv("TWITTER_API_KEY"), consumer_secret=os.getenv("TWITTER_API_SECRET_KEY"),
         access_token=os.getenv("TWITTER_ACCESS_TOKEN"), access_token_secret=os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
         )
