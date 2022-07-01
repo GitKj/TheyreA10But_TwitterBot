68fffb996653bf31728504bc3652fa3c8c63ba97
Kajoyrie P <32532509+GitKj@users.noreply.github.com>
2022-06-30 18:35:35 -0700

    removed config import

--- twitter_client.py
+++ twitter_client.py
@@ -1,15 +1,8 @@
 import tweepy
 import os
-import config
 
 def get_Twitter_Client():
     return tweepy.Client(
         consumer_key=os.getenv("TWITTER_API_KEY"), consumer_secret=os.getenv("TWITTER_SECRET_KEY"),
         access_token=os.getenv("TWITTER_ACCESS_TOKEN"), access_token_secret=os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
         )
-
-def get_temp_client():
-    return tweepy.Client(
-        consumer_key=config.TWITTER_API_KEY, consumer_secret=config.TWITTER_API_SECRET_KEY,
-        access_token=config.TWITTER_ACCESS_TOKEN, access_token_secret=config.TWITTER_ACCESS_TOKEN_SECRET
-    )
